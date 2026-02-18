fe0cc7ed7a7b3e00e18b8eb24b544cc708aeeb7f
noob-prachurjo <prachurjobarmon2008@gmail.com>
2026-02-18 16:06:52 +0600

    login form working successfully

---
+++ script/login.js
@@ -0,0 +1,17 @@
+console.log("Login page script loaded");
+
+document.getElementById('login').addEventListener('click', function(){
+    const number = document.getElementById('input-num');
+    const numValue = number.value;
+    console.log(numValue);
+
+    const pin = document.getElementById('input-pin');
+    const pinValue = pin.value;
+    console.log(pinValue);
+
+    if (numValue == "01705830946" && pinValue == "2008"){
+        alert("Login Successfull !")
+    } else{
+        alert("Login Falied !")
+    }
+})
